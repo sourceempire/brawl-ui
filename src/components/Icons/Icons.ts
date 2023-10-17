6e0c692a9683e9315e32acb6d816793603be2736
Jonas Gustafson <jonasgustafson@hotmail.se>
2023-10-17 20:25:15 +0200

    added more icons

--- src/components/Icons/Icons.ts
+++ src/components/Icons/Icons.ts
@@ -4,3 +4,12 @@ export { LuBell as Bell } from "react-icons/lu";
 export { LuUsers as Users } from "react-icons/lu"
 export { LuUserMinus as UserMinus } from "react-icons/lu"
 export { LuUserPlus as UserPlus } from "react-icons/lu"
+
+export { LuPartyPopper as PartyPopper } from "react-icons/lu";
+export { LuPlus as Plus } from "react-icons/lu";
+export { LuChevronDown as ChevronDown } from "react-icons/lu";
+export { LuChevronUp as ChevronUp } from "react-icons/lu";
+export { LuX as Cross } from "react-icons/lu";
+export { LuStar as Star } from "react-icons/lu"
+
+
