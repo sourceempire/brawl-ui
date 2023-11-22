b7fc5183ca0784412e31c66de795ce9d633a9086
Jonas Gustafson <jonasgustafson@hotmail.se>
2023-11-22 18:19:34 +0100

    added function to create types index

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,16 @@ import react from '@vitejs/plugin-react'
 import autoprefixer from 'autoprefixer'
 import dts from 'vite-plugin-dts';
 
+import fs from 'fs';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts({
     outDir: 'dist/types',
     include: ["src/components/*/*.{ts,tsx}", "src/components/index.ts", "src/context/*.{ts,tsx}", "src/context/index.ts"],
+    afterBuild: createTypesIndex,
     // beforeWriteFile: async (filePath, content) => {      
+    //   console.log({filePath, content})
     //   if (filePath.includes('/components')) {
     //     filePath = filePath.replace('/components', '');
     //   } else if (filePath.includes('/context')) {
@@ -37,3 +41,13 @@ export default defineConfig({
     }
   }
 })
+
+function createTypesIndex() {
+  const files = ['components', 'context']; // You need to populate this array with paths to your .d.ts files
+  let indexContent = "";
+  files.forEach((file) => {
+    const exportLine = `export * from './${file}';\n`;
+    indexContent += exportLine;
+  });
+  fs.writeFileSync("dist/types/index.d.ts", indexContent);
+}
